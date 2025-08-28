884d768ad64d50f1b27b5a9350ac62ff1665c78e
TalkoDA <azzi2710@gmail.com>
2025-08-28 18:15:42 +0200

    fixed readme

--- stats.rb
+++ stats.rb
@@ -4,8 +4,8 @@ require "descriptive_statistics"
 
 data = [5, 10, 10, 20, 25, 30]
 
-puts "Данные: #{data}"
-puts "Среднее: #{data.mean}"
-puts "Медиана: #{data.median}"
-puts "Мода: #{data.mode}"
-puts "Стандартное отклонение: #{data.standard_deviation.round(2)}"
+puts "Data: #{data}"
+puts "Mean: #{data.mean}"
+puts "МMedian: #{data.median}"
+puts "Mode: #{data.mode}"
+puts "Standart deviation: #{data.standard_deviation.round(2)}"
