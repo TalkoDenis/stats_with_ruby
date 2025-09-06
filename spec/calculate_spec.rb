612b8f95cff9313120fd72698b2275ab33cf8043
TalkoDA <azzi2710@gmail.com>
2025-09-06 11:50:11 +0200

    add tests

--- spec/calculate_spec.rb
+++ spec/calculate_spec.rb
@@ -3,22 +3,41 @@
 require "stats_with_ruby/calculate"
 
 RSpec.describe "Stats functions" do
-  let(:data) { [1, 2, 2, 3, 3, 3, 4] }
+  let(:data) { [1, 2, 2, 3, 3, 3, 4, 2, 2, -1, -2, -3, 5, 8, 12, 3, 3, 2, 1, 4, 6, 3, -5, 3] }
 
-  it "find MIN" do
-    expect(min(data)).to eq(1)
+  it "find MIN value" do
+    expect(min(data)).to eq(-5)
   end
 
-  it "find MAX" do
-    expect(max(data)).to eq(4)
+  it "find MAX value" do
+    expect(max(data)).to eq(12)
   end
 
-  it "calculate AVG" do
-    expect(mean(data)).to eq(18.0 / 7)
+  it "find AVG value" do
+    expect(mean(data).round(2)).to eq(2.54)
   end
 
-  it "find MODE" do
+  it "find MODE value" do
     expect(mode(data)).to eq(3)
   end
-end
 
+  it "find Median value" do
+    expect(median(data)).to eq(3.0)
+  end
+
+  it "find Sum value" do
+    expect(sum(data)).to eq(61)
+  end
+
+  it "find Variance value" do
+    expect(variance(data).round(2)).to eq(11.25)
+  end
+
+  it "find Range value" do
+    expect(range(data)).to eq(17)
+  end
+
+  it "find Standard deviation value" do
+    expect(standard_deviation(data).round(2)).to eq(3.35)
+  end
+end
